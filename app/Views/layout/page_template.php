72356b7da719f4eba3308d2a7dd6108470793ddc
MaulIshak <98244927+MaulIshak@users.noreply.github.com>
2025-09-12 21:02:52 +0700

    add view layout (not implemented)

---
+++ app/Views/layout/page_template.php
@@ -0,0 +1,17 @@
+<?= $this->extends('base_template') ?>
+<?= $this->section('content') ?>
+
+  <nav>
+
+  </nav>
+
+  <main>
+    <?= $this->renderSection('main-content')?>
+  </main>
+
+  <footer>
+
+  </footer>
+
+
+<?= $this->endsection() ?>
